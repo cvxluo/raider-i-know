7a020c76ee85a39242d4b2aacb0fc6f42f83171a
Charlie Luo <cvxluo@gmail.com>
2024-04-02 05:46:07 -0400

    More analysis on title-level keys

--- analysis/consts.py
+++ analysis/consts.py
@@ -40,3 +40,20 @@ DUNGEON_IDS = [
   4738, # throne
   9424, # waycrest
 ]
+
+CLASS_COLORS = {
+  "Death Knight": "#C41F3B",
+  "Demon Hunter": "#A330C9",
+  "Druid": "#FF7D0A",
+  "Evoker": "#33937F",
+  "Hunter": "#ABD473",
+  "Mage": "#40C7EB",
+  "Monk": "#00FF96",
+  "Paladin": "#F58CBA",
+  # "Priest": "#FFFFFF",
+  "Priest": "#808080",
+  "Rogue": "#FFF569",
+  "Shaman": "#0070DE",
+  "Warlock": "#8787ED",
+  "Warrior": "#C79C6E",
+};
